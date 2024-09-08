7eeb5905dfa2d0488254333db861036344bdf8c3
rongchen0 <rongmrok@gmail.com>
2024-09-08 22:42:08 +0800

    add CommandLineRunner

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,8 +2,10 @@ package com.example.demo;
 
 import com.example.demo.bean.TestProperties;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 //import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,4 +28,10 @@ public class DemoApplication {
 //		System.out.println(testProperties);
 //		return "hello world";
 //	}
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return (args) -> {
+			System.out.println(testProperties);
+		};
+	}
 }
